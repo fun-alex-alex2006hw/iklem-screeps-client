2f6930a5d88cc18b57c8ea404925e3afd4575679
Clément Delalande <iklem.d@gmail.com>
2017-01-10 11:43:35 +0100

    updated storing of servers

--- app/scripts/addServer.js
+++ app/scripts/addServer.js
@@ -1,5 +1,27 @@
-function addServer($scope, $http, $location) {
-  $scope.back = () => {
-    $location.path("/");
+function addServer($scope, $location, serverListService) {
+  $scope.server = {
+    name: "",
+    ip: "",
+    port: 21025,
+    hasOtherAuthSys: false
+  }
+
+  $scope.back = () => $location.path("/");
+
+  $scope.add = () => {
+    let server = $scope.server;
+    if (server.ip !== "") {
+      console.log(`Adding ${server.ip}`);
+      alert(`Adding ${server.ip}`);
+      serverListService.addServer(server);
+      fs.writeFileSync(
+        "app/servers.json",
+        `${JSON.stringify(serverListService.getServerList(true))}`
+      );
+      $scope.back();
+    } else {
+      alert("You need to provide the IP address");
+      console.log("Can't add a server with no IP");
+    }
   }
 }
